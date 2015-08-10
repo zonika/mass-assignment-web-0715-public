4d49f57b685fe0bc8998b8e6a23483d34f50b3af
zonika <zchodosh@conncoll.edu>
2015-08-10 18:05:09 -0400

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-  # code here
+  def initialize(attributes)
+    attributes.each do |property,value|
+      #instance_variable_set("@#{property}",value)
+      self.send("#{property}=",value)
+    end
+  end
+  [:name, :birthday, :hair_color, :eye_color, :height, :weight,
+    :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size,
+    :pant_length, :pant_width].each do |meth|
+    define_method meth do
+      instance_variable_get("@#{meth}")
+    end
+    define_method "#{meth}=" do |new_value|
+      instance_variable_set("@#{meth}", new_value)
+    end
+  end
 end
